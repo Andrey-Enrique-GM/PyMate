1754c4a9c643042e94ee3a09a111a17b9d999986
Andrey <andreyenrique132@gmail.com>
2026-09-14 17:45:33 -0700

    06, correccion a la velocidad de los frames de goldship

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from core.window import PetWindow
 def main():
     app = QApplication(sys.argv)
 
-    # Ruta a la animación idle (con 340 frames)
     sprite_path = "assets/SpriteSheet/GoldShip/idle.png"
 
-    pet = PetWindow(sprite_path=sprite_path, total_frames=340, fps=60)
+    pet = PetWindow(sprite_path=sprite_path, total_frames=340, target_size=230, fps=35)
     pet.show()
 
     sys.exit(app.exec())
--- core/sprite_animator.py
+++ core/sprite_animator.py
@@ -2,7 +2,7 @@ from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import QRect
 
 class SpriteAnimator:
-    def __init__(self, sprite_path: str, total_frames: int, frame_width: int = 350, frame_height: int = 350, columns: int = 10):
+    def __init__(self, sprite_path: str, total_frames: int = 340, frame_width: int = 350, frame_height: int = 350, columns: int = 10):
         self.full_pixmap = QPixmap(sprite_path)
         self.total_frames = total_frames
         self.frame_width = frame_width
@@ -11,14 +11,13 @@ class SpriteAnimator:
         self.current_frame = 0
 
         if self.full_pixmap.isNull():
-            print(f"Error: No se pudo cargar el archivo de sprites: {sprite_path}")
+            print(f"Error cargando: {sprite_path}")
 
     def get_next_frame(self) -> QPixmap:
-        """Avanza al siguiente cuadro y devuelve la sub-imagen correspondiente."""
         if self.full_pixmap.isNull():
             return QPixmap()
 
-        # Calcular fila y columna
+        # Matriz de 10 columnas
         col = self.current_frame % self.columns
         row = self.current_frame // self.columns
 
@@ -27,6 +26,5 @@ class SpriteAnimator:
 
         cropped = self.full_pixmap.copy(QRect(x, y, self.frame_width, self.frame_height))
 
-        # Incrementar frame y reiniciar al llegar al límite de la animación
         self.current_frame = (self.current_frame + 1) % self.total_frames
         return cropped
